e84530b4314fb2f6caf621c1cd29506087c3974b
Anders Lauvrak Espenes <andersle@uia.no>
2020-11-02 21:19:23 +0100

    Fixed importing bug, added more water, waterfall and improved wall bitmap

--- client/Scenes/TestMap/TileMap.gd
+++ client/Scenes/TestMap/TileMap.gd
@@ -8,7 +8,9 @@ var delay = 1.0
 
 var region_x = 0
 
-var size = 256 * 8 # Size of one set * number of animation frames
+var size = 256 * 4 # Size of one set * number of animation frames
+
+var tile_sets = null
 
 
 # Called when the node enters the scene tree for the first time.
@@ -20,14 +22,16 @@ func _ready():
 	timer.set_wait_time(delay)	# Set timer delay
 	timer.set_one_shot(false) # Make timer loop
 	timer.start()		# Start timer
+	
+	tile_sets = tile_set.get_tiles_ids() # Get id of all the tilesets
 
 func _on_Timer_timeout():
 	delay = rand_range(0.5, 1.2) # Create random intervals for timer
 	timer.set_wait_time(delay)	# Set new delay on timer
 	region_x += 256			# Move over one frame
 	region_x %= size	# Goes to 0 when region_x is equal to size
-	tile_set.tile_set_region(0, Rect2(region_x, 0.0, 256.0, 192)) # Rectangle(start_x, start_y, width, height)
-	tile_set.tile_set_region(1, Rect2(region_x, 0.0, 256.0, 192)) # Rectangle(start_x, start_y, width, height)
-	tile_set.tile_set_region(2, Rect2(region_x, 0.0, 256.0, 192)) # Rectangle(start_x, start_y, width, height)
-	tile_set.tile_set_region(3, Rect2(region_x, 0.0, 256.0, 192)) # Rectangle(start_x, start_y, width, height)
-	tile_set.tile_set_region(4, Rect2(region_x, 0.0, 256.0, 192)) # Rectangle(start_x, start_y, width, height)
+	
+	for set in tile_sets:
+		tile_set.tile_set_region(set, Rect2(region_x, 0.0, 256.0, 192)) # Move the tile region
+	
+	
